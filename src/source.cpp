a22c66361f4b442983ed61123a5e94187065f951
anatoly <tolya_ageev@bk.ru>
2020-05-06 13:03:13 +0700

    Create new function - hello

---
+++ src/source.cpp
@@ -0,0 +1,30 @@
+#include "source.h"
+
+using namespace std;
+
+void hello()
+{
+    cout << "Hello, dear user! You have problems with "
+            "English and want to learn irregular verbs?"
+         << endl;
+    cout << "Welcome to the A. A. A. team!" << endl;
+    cout << "We, Ageev Anatoly, Tverdokhleb Anton and tsikunov Andrey, "
+            "students "
+            "Sibguti group IP-913."
+         << endl;
+    cout << "We have volunteered to help you brighten up your evening studying "
+            "English "
+            "language!"
+         << endl;
+    cout << endl
+         << "This program is designed to hone your knowledge "
+            "irregular verb."
+         << endl;
+    cout << endl
+         << "We wish you good luck in learning verbs! Click any button to "
+            "resume."
+         << endl
+         << endl;
+    _getch();
+    system("CLS");
+}
