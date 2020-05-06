da4a628b781cb24dd01d3695e2ba4b6316d58872
anatoly <tolya_ageev@bk.ru>
2020-05-06 14:16:20 +0700

    Create new funclion - test1

--- src/source.cpp
+++ src/source.cpp
@@ -54,3 +54,61 @@ void support()
     _getch();
     system("CLS");
 }
+
+void KoE(string v[4][n])
+{
+    int score = 0, sc = 0, k, Oshibki = 0;
+    int t = clock();
+    int timer = clock() - t;
+    int timeAll = 0;
+    string vUser[3];
+    for (int i = 0; score < 10; i++) {
+        k = rand() % n;
+        cout << "Введите все формы слова " << v[3][k] << " (" << v[0][k] << ") "
+             << endl;
+        for (i = 0; i < 3; i++) {
+            cin >> vUser[i];
+            if (vUser[i] == "exit") {
+                system("cls");
+                t = 0;
+                timer = 0;
+                timeAll = 0;
+                mainMenu(v);
+                break;
+            }
+        }
+        for (i = 0; i < 3; i++) {
+            if (v[i][k] == vUser[i]) {
+                sc++;
+            }
+        }
+        if (sc == 3) {
+            cout << "Верно! Продолжайте в том же духе!";
+            score++;
+            sc = 0;
+        }
+
+        else {
+            cout << "Ошибка! Правильный ответ: ";
+            sc = 0;
+            for (i = 0; i < 3; i++) {
+                cout << v[i][k] << " ";
+            }
+            Oshibki++;
+        }
+        cout << endl;
+
+        timer = clock() - t;
+        t = clock();
+        timeAll += timer;
+        cout << "Время, которое ушло на этот глагол: " << timer / 1000
+             << " секунд" << endl;
+        cout << "Ваш счёт: " << score
+             << "\t Количество допущенных ошибок: " << Oshibki << endl
+             << endl;
+    }
+
+    cout << endl
+         << "Общее время выполнения задания: " << timeAll / 1000 << " секунд";
+    //  ToRIn()
+}
