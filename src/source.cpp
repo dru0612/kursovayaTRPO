6ed67982e83f978261fa7c5c2f249da828e417d4
dru0612 <dru0612@mail.ru>
2020-05-12 15:17:09 +0700

    Fix format description travis1

--- src/source.cpp
+++ src/source.cpp
@@ -47,21 +47,21 @@ void support(string v[4][n])
          << endl;
     cout << endl;
     cout << "Test for knowledge of all forms goes until you score 100 points"
-        << endl;
-   cout << "15 points are given for the correct answer, and 5 points are "
+         << endl;
+    cout << "15 points are given for the correct answer, and 5 points are "
             "taken for the wrong answer"
-        << endl
-        << endl;
-   cout << "In the test for time, the task is to score the maximum number of "
+         << endl
+         << endl;
+    cout << "In the test for time, the task is to score the maximum number of "
             "points in 60 seconds, the user must enter 3 forms of the word."
-        << endl;
-   cout << "For the correct answer is given 20 points." << endl;
-   cout << endl;
-   cout << "In the translation knowledge test, 4 translations are proposed, "
+         << endl;
+    cout << "For the correct answer is given 20 points." << endl;
+    cout << endl;
+    cout << "In the translation knowledge test, 4 translations are proposed, "
               "one of which is correct."
-        << endl;
-   cout << "For the right choise +10 points, for the wrong - 5 points" << endl;
-   cout << endl
+         << endl;
+    cout << "For the right choise +10 points, for the wrong - 5 points" << endl;
+    cout << endl
          << "We wish you good luck in learning verbs! Click any button to "
             "resume."
          << endl
