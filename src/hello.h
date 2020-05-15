30f313ffaa0c972310875c3d6553ee78abd87575
anatoly <tolya_ageev@bk.ru>
2020-05-15 18:10:41 +0700

    Create heder for hello.cpp

---
+++ src/hello.h
@@ -0,0 +1,17 @@
+#ifndef HEL_H
+#define Hel_H
+
+#include <fstream>
+#include <iostream>
+#include <locale.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+using namespace std;
+
+void hello();
+void support(string v[4][100]);
+
+#endif
